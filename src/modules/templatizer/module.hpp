216782c62b630945df3889bbe63bdb941770f07f
DmitryKuk <d1021976@yandex.ru>
2015-12-09 18:00:29 +0300

    Code style fixed

--- src/modules/templatizer/module.hpp
+++ src/modules/templatizer/module.hpp
@@ -7,7 +7,8 @@ templatizer::module<ModuleRepr>::module(const std::string &command,
 										templatizer::module_registrar &registrar):
 	base::module<std::string, templatizer::chunk_generator_type>{
 		command,
-		[](const std::string &argument) -> std::unique_ptr<templatizer::chunk> {
+		[](const std::string &argument) -> std::unique_ptr<templatizer::chunk>
+		{
 			return std::make_unique<ModuleRepr>(argument);
 		},
 		registrar

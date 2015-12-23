7e717a9f71402959783cc758498a8e0e5a9cd6ba
DmitryKuk <d1021976@yandex.ru>
2015-12-23 15:09:20 +0300

    File type determination simplified and improved

--- src/modules/system_/utils.cpp
+++ src/modules/system_/utils.cpp
@@ -2,6 +2,8 @@
 
 #include <system_/utils.h>
 
+#include <unordered_map>
+
 #include <cstdio>
 
 #include <system_/exceptions.h>
@@ -11,9 +13,23 @@ using namespace std::literals;
 
 namespace {
 
+
 constexpr size_t max_output_len = 256;	// Including trailing '\0'
 
+
+using p = std::pair<std::string, std::string>;
+
+std::unordered_map<std::string, std::string> mime_types_by_extensions{
+	{
+		p{"html"s,	"text/html"s },
+		p{"css"s,	"text/css"s  },
+		p{"txt"s,	"text/plain"s}
+	}
+};
+
+
 };	// namespace
+
 
 
 // Returns given file mime type as a string or throws system_::unknown_error.
@@ -21,6 +37,16 @@ std::string
 system_::get_file_mime(const std::string &path,
 					   const std::string &command)
 {
+	// Try to analyze file extension
+	size_t pos = path.find_last_of("./"s);
+	if (pos != std::string::npos && path[pos] == '.') {
+		auto it = mime_types_by_extensions.find(path.substr(pos + 1));	// Search for extension
+		if (it != mime_types_by_extensions.end())
+			return it->second;
+	}
+	
+	
+	// Ask external utility
 	std::string command_full = command + " \'"s + path + '\'';
 	std::FILE *stream = ::popen(command_full.c_str(), "r");
 	char str[max_output_len];
